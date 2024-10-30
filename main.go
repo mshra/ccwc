dd12c7e572b9f268fefa9f138756a43a91df9965
Aaryan Mishra <aaryan.mshra@gmail.com>
2024-10-30 13:38:08 +0530

    fin.

--- main.go
+++ main.go
@@ -8,30 +8,62 @@ import (
 	"os"
 )
 
-var isNumberOfByteModeEnabled bool
-var isNumberOfLineModeEnabled bool
-var fileName string
+var (
+	fileName                   string
+	isNumberOfByteModeEnabled  bool
+	isNumberOfLineModeEnabled  bool
+	isNumberOfWordsModeEnabled bool
+)
 
 func init() {
-	flag.BoolVar(&isNumberOfByteModeEnabled, "c", false, "read number of bytes in the given file")
-	flag.BoolVar(&isNumberOfLineModeEnabled, "l", false, "read number of lines in the given file")
+	flag.BoolVar(&isNumberOfByteModeEnabled, "c", false, "read number of fileToReadNumberOfBytes in the given file")
+	flag.BoolVar(&isNumberOfLineModeEnabled, "l", false, "read number of fileToReadNumberOfLines in the given file")
+	flag.BoolVar(&isNumberOfWordsModeEnabled, "m", false, "read number of fileToReadNumberofWords in the given file")
 
 	flag.Parse()
 }
 
 func main() {
 	fileName = os.Args[len(os.Args)-1]
-	file, err := os.Open(fileName)
-	checkError(err)
-	defer file.Close()
 
 	if isNumberOfByteModeEnabled {
-		fmt.Println(getFileStat(file).Size(), fileName)
+		file, err := os.Open(fileName)
+		checkError(err)
+		defer file.Close()
+		fmt.Println(getNumberOfBytesInFile(file), fileName)
+		return
 	}
 
-	if isNumberOfByteModeEnabled {
+	if isNumberOfLineModeEnabled {
+		file, err := os.Open(fileName)
+		checkError(err)
+		defer file.Close()
 		fmt.Println(getNumberOfLinesInFile(file), fileName)
+		return
 	}
+
+	if isNumberOfWordsModeEnabled {
+		file, err := os.Open(fileName)
+		checkError(err)
+		defer file.Close()
+		fmt.Println(getNumberOfWordsInFile(file), fileName)
+		return
+	}
+
+	fileToReadNumberOfBytes, err := os.Open(fileName)
+	checkError(err)
+	defer fileToReadNumberOfBytes.Close()
+
+	fileToReadNumberOfLines, err := os.Open(fileName)
+	checkError(err)
+	defer fileToReadNumberOfLines.Close()
+
+	fileToReadNumberofWords, err := os.Open(fileName)
+	checkError(err)
+	defer fileToReadNumberofWords.Close()
+
+	fmt.Println(getNumberOfLinesInFile(fileToReadNumberOfLines), getNumberOfWordsInFile(fileToReadNumberofWords), getNumberOfBytesInFile(fileToReadNumberOfBytes), fileName)
+	return
 }
 
 func checkError(err error) {
@@ -40,14 +72,14 @@ func checkError(err error) {
 	}
 }
 
-func getFileStat(f *os.File) os.FileInfo {
+func getNumberOfBytesInFile(f *os.File) int64 {
 	fileStat, err := f.Stat()
 	checkError(err)
-	return fileStat
+	return fileStat.Size()
 }
 
-func getNumberOfLinesInFile(f *os.File) int {
-	var lineCount int
+func getNumberOfLinesInFile(f *os.File) int64 {
+	var lineCount int64
 	scanner := bufio.NewScanner(f)
 
 	for scanner.Scan() {
@@ -55,4 +87,17 @@ func getNumberOfLinesInFile(f *os.File) int {
 	}
 
 	return lineCount
+}
+
+func getNumberOfWordsInFile(f *os.File) int {
+	wordsCount := 0
+	scanner := bufio.NewScanner(f)
+	scanner.Split(bufio.ScanWords)
+
+	for scanner.Scan() {
+		wordsCount++
+	}
+	checkError(scanner.Err())
+
+	return wordsCount
 }
